7b665b7f22cd2b4210e494cc149366940eff6701
Spencer Hivert <spencer.hivert@gmail.com>
2019-01-28 19:37:12 -0500

    MERM-000: Fix Login

--- app/graphql/types/auth_type.rb
+++ app/graphql/types/auth_type.rb
@@ -6,6 +6,6 @@ module Types
 
     field :authenticationToken, !types.String, property: :authentication_token
     field :firstName, !types.String, property: :first_name
-    field :lastLame, !types.String, property: :last_name
+    field :lastName, !types.String, property: :last_name
   end
 end
