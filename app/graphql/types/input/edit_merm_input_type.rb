8f6a84addd8a50b25b664b4772b49ff3be7909ea
Spencer Hivert <spencer.hivert@gmail.com>
2019-03-08 14:51:38 -0500

    MERM-000: Add Elastic initializer

--- app/graphql/types/input/edit_merm_input_type.rb
+++ app/graphql/types/input/edit_merm_input_type.rb
@@ -5,12 +5,12 @@ module Types
       description 'Properties for editing a Merm'
 
       # argument :name, !types.String,
-      argument :name, !types.String
-      argument :resourceName, !types.String
+      argument :name, types.String
+      argument :resourceName, types.String
       argument :capturedText, types.String
-      argument :resourceUrl, !types.String
-      argument :description, !types.String
-      argument :categoryId, !types.ID
+      argument :resourceUrl, types.String
+      argument :description, types.String
+      argument :categoryId, types.ID
     end
   end
 end
--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,3 +1,7 @@
-Elasticsearch::Model.client = Elasticsearch::Client.new({
-                                                            log: true
-                                                        })
+client = Elasticsearch::Client.new({
+                              log: true
+                          })
+
+client.indices.put_settings body: { 'index.blocks.read_only_allow_delete' => "null"}
+
+Elasticsearch::Model.client = client
